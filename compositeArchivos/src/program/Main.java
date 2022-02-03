aaab6c9342e262022f979d763cf79cd9e807cbfd
Agustin Grigaliunas <agustingrigaliunas@MacBook-Pro-de-Agustin.local>
2022-02-03 17:14:10 -0300

    main

--- compositeArchivos/src/program/Main.java
+++ compositeArchivos/src/program/Main.java
@@ -1,10 +1,38 @@
 package program;
 
+import domain.Archivo;
+import domain.Carpeta;
+import domain.Unidad;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Carpeta carpetaPrimaria = new Carpeta();
+		Carpeta carpetaUno = new Carpeta();
+		Carpeta carpetaDos = new Carpeta();
+		
+		Archivo archivoUno = new Archivo(100);
+		Archivo archivoDos = new Archivo(212);
+		Archivo archivoTres = new Archivo(35);
+		Archivo archivoCuatro = new Archivo(90);
+		Archivo archivoCinco = new Archivo(5);
 
+
+		carpetaUno.agregarUnidad(archivoUno);
+		carpetaUno.agregarUnidad(archivoDos);
+		carpetaUno.agregarUnidad(archivoTres);
+		
+		carpetaDos.agregarUnidad(archivoCuatro);
+
+		carpetaPrimaria.agregarUnidad(carpetaUno);
+		carpetaPrimaria.agregarUnidad(carpetaDos);
+		carpetaPrimaria.agregarUnidad(archivoCinco);
+
+		System.out.println("Carpeta primaria" + carpetaPrimaria.tamanoTotal());
+		System.out.println("Carpeta uno: " + carpetaUno.tamanoTotal());
+		System.out.println("Carpeta dos: " + carpetaDos.tamanoTotal());
+
+	}
 	}
 
 }
