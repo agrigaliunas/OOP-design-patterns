b07af0a804b34b226f68c664d53e70a52a2e75ac
Agustin Grigaliunas <agustingrigaliunas@MacBook-Pro-de-Agustin.local>
2022-02-03 19:27:09 -0300

    GerenteGeneral class done

--- compositeEmpleados/src/domain/GerenteGeneral.java
+++ compositeEmpleados/src/domain/GerenteGeneral.java
@@ -1,11 +1,32 @@
 package domain;
 
-public class GerenteGeneral implements Empleado {
+import java.util.ArrayList;
+import java.util.List;
 
+public class GerenteGeneral implements Empleado {
+	List<Empleado> ListaEmpleadosACargo;
+	
+	public GerenteGeneral(List<Empleado> empleadosACargo){
+		this.ListaEmpleadosACargo = new ArrayList<>();
+	}
+	
+	public void agregarEmpleadoACargo(Empleado empleadoNuevo) {
+		ListaEmpleadosACargo.add(empleadoNuevo);
+	}
+	
+	public void eliminarEmpleadoACargo(Empleado empleadoNuevo) {
+		ListaEmpleadosACargo.remove(empleadoNuevo);
+	}
+	
 	@Override
-	public int obtenerEmpleadosACargo() {
-		// TODO Auto-generated method stub
-		return 0;
+	public int obtenerEmpleadosACargo() { // recursiva
+		int empleadosACargo = 0;
+		
+		for(Empleado empleado: ListaEmpleadosACargo) {
+			empleadosACargo += empleado.obtenerEmpleadosACargo();
+		}
+		
+		return empleadosACargo;
 	}
 
 }
