13de8bfeb02eed22f8a1e759ec60b9d442ed02ea
Agustin Grigaliunas <agustingrigaliunas@gmail.com>
2022-02-06 16:58:03 -0300

    Cuenta class constructor

--- strategyRestaurante/src/domain/Cuenta.java
+++ strategyRestaurante/src/domain/Cuenta.java
@@ -1,5 +1,14 @@
 package domain;
 
 public class Cuenta {
+	private double totalConsumido;
+	private Descuento descuentoAplicado;
+	private FormaPago formaDePago;
 
+	public Cuenta(double consumo, FormaPago formaDePago) {
+		this.totalConsumido = consumo;
+		this.formaDePago = formaDePago;
+		this.descuentoAplicado = new DescuentoOtrosDias(); // supongamos que es lunes
+	}
+	
 }
--- strategyRestaurante/src/program/Main.java
+++ strategyRestaurante/src/program/Main.java
@@ -1,9 +1,14 @@
 package program;
 
+import domain.*;
+
 public class Main {
 	
 	public static void main(String[] args) {
-		Cuenta cuenta = new Cuenta();
+		FormaPago efectivo = new Efectivo();
+		FormaPago tarjeta = new Tarjeta();
+		
+		Cuenta cuenta = new Cuenta(2600, efectivo);
 				
 	}
 
